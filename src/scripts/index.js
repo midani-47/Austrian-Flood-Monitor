cd03b6b59016258e9f4ed0b240773329e703f739
saraylaszlo <23IMC10636@fh-krems.ac.at>
2024-11-30 00:03:54 +0100

    Changes to the gener homepage

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,14 @@
 // Initialize the Leaflet map
 const map = L.map('map', {
   zoomControl: false, // Disable default zoom controls
-}).setView([47.5162, 14.5501], 7); // Center of Austria
+}).setView([47.7162, 14.5501], 7); // Center of Austria
 
-// Add OpenStreetMap tiles
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+const apiKey = "The_api_key"
+
+// Add Corto's tiles
+L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=${apiKey}', {
+  attribution: '&copy; <a href="https://www.maptiler.com/">MapTiler</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  maxZoom: 19,
 }).addTo(map);
 
 // Add custom zoom controls below the navbar
@@ -19,8 +22,10 @@ const bounds = [
   [49.5, 17.5],  // Northeast corner
 ];
 map.setMaxBounds(bounds);
-map.setMinZoom(7); // Prevent zooming out too far
+map.setMinZoom(8); // Prevent zooming out too far
 map.setMaxZoom(11); // Prevent excessive zooming in
+
+
 
 // Add a marker for the center of Austria
 L.marker([47.5162, 14.5501]).addTo(map).bindPopup('Center of Austria').openPopup();
