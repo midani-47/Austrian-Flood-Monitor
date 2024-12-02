3314e4eebb3aed607ae99670f258c8703ae9da86
saraylaszlo <23IMC10636@fh-krems.ac.at>
2024-12-02 19:02:29 +0100

    Adding the base of the map

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,13 +3,12 @@ const map = L.map('map', {
   zoomControl: false, // Disable default zoom controls
 }).setView([47.7162, 14.5501], 7); // Center of Austria
 
-const apiKey = "The_api_key"
-
-// Add Corto's tiles
-L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=${apiKey}', {
+// Add MapTiler Basic Map tiles
+L.tileLayer('https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=THE_API_KEY', {
   attribution: '&copy; <a href="https://www.maptiler.com/">MapTiler</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   maxZoom: 19,
 }).addTo(map);
+
 
 // Add custom zoom controls below the navbar
 L.control.zoom({
@@ -25,7 +24,10 @@ map.setMaxBounds(bounds);
 map.setMinZoom(8); // Prevent zooming out too far
 map.setMaxZoom(11); // Prevent excessive zooming in
 
+// Load GeoJSON data and add as a layer
+// This needs to be finished while Beutifiying the map
 
 
 // Add a marker for the center of Austria
+// This is just a demo marker
 L.marker([47.5162, 14.5501]).addTo(map).bindPopup('Center of Austria').openPopup();
